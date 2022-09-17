c57e3bcbf5321d308acf7dce9e4ce5a9c1946a46
erickaltamirano07 <79813692+erickaltamirano07@users.noreply.github.com>
2022-09-17 15:55:10 -0500

    CapaBusiness

---
+++ Business/B_Category.cs
@@ -0,0 +1,49 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccess;
+using System.Linq;
+
+namespace Business
+{
+    public class B_Category
+    {
+        public List<CategoryEntity>CategoryList()
+        {
+            using (var db = new InventaryContext())
+            {
+                return db.Categories.ToList();
+            }
+        }
+
+        public void CreateCategory(CategoryEntity category)
+        {
+            using (var db = new InventaryContext())
+            {
+                db.Categories.Add(category);
+                db.SaveChanges();
+
+            }
+        }
+
+        public void UpdateCategory(CategoryEntity category)
+        {
+            using (var db = new InventaryContext())
+            {
+                db.Categories.Update(category);
+                db.SaveChanges();
+            }
+        }
+
+        public void RemoveCategory(CategoryEntity category)
+        {
+            using (var db = new InventaryContext())
+            {
+                db.Categories.Remove(category);
+                db.SaveChanges();
+            }
+        }
+
+    }
+}
---
+++ DataAccess/Migrations/20220917201956_AddData.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class AddData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[,]
+                {
+                    { "ASH", "Aseo Hogar" },
+                    { "ASP", "Aseo Personal" },
+                    { "HGR", "Hogar" },
+                    { "PRF", "Perfumería" },
+                    { "SLD", "Salud" },
+                    { "VDJ", "Video Juegos" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Warehouses",
+                columns: new[] { "WarehouseId", "WarehouseAddress", "WarehouseName" },
+                values: new object[,]
+                {
+                    { "6b0e5812-fa56-468c-8ba6-ad2a52fba362", "Calle 8 con 23", "Bodega Central" },
+                    { "20b12b0d-b2c2-4655-892b-b8fa490d1947", "Calle Norte con Occiedente", "Bodega Norte" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "ASH");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "ASP");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "HGR");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "PRF");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "SLD");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "VDJ");
+
+            migrationBuilder.DeleteData(
+                table: "Warehouses",
+                keyColumn: "WarehouseId",
+                keyValue: "20b12b0d-b2c2-4655-892b-b8fa490d1947");
+
+            migrationBuilder.DeleteData(
+                table: "Warehouses",
+                keyColumn: "WarehouseId",
+                keyValue: "6b0e5812-fa56-468c-8ba6-ad2a52fba362");
+        }
+    }
+}
